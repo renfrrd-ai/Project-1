b07447de31656b6d51ed7be5dd0440483a4b290b
renfrrd.ai <alongerenfred@gmail.com>
2025-11-24 21:55:54 -0500

    Adding the summarize endpoint to Flask

--- ml-email-classifier/src/api/app.py
+++ ml-email-classifier/src/api/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, request, jsonify
 import joblib
-from src.basic_text_playground import clean_text
+from src.basic_text_playground import clean_text, summarize_email
 from pathlib import Path
+import nltk
+from nltk.corpus import stopwords
 
 app = Flask(__name__)
 
@@ -19,6 +21,14 @@ def predict():
     pred = model.predict(X)
 
     return jsonify(pred[0])
+
+
+@app.route("/summarize", methods=["POST"])
+def summarize():
+    text = request.json["text"]
+    summary = summarize_email(text)
+
+    return jsonify({"summary": summary})
 
 
 if __name__ == "__main__":
